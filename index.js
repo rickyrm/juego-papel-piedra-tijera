6a2620e0abe0934377886edac41e289d7c226c10
Ricardo Rivero <riki.1997.121@gmail.com>
2024-11-13 20:45:59 +0000

    Información en el html del ressultado

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 
-var jugador1 = prompt ("Jugador 1, inserte piedra, papel o tijeras: ");
-var jugador2 = prompt ("Jugador 2, inserte piedra, papel o tijeras: ");
+var jugador1 = prompt("Jugador 1 inserte piedra, papel o tijeras: ");
+var jugador2 = prompt("Jugador 2 inserte piedra, papel o tijeras: ");
 
+// Variable que funciona como contenedor para mostrar el resultado en el html
+var contenedorResultado = document.getElementById("resultado");
 
-if (jugador1.toLowerCase() == jugador2.toLowerCase() )
-{
-alert ("Empate");
-}
-
-else if ((jugador1.toLowerCase() == "papel" && jugador2.toLowerCase() =="piedra") ||
-    (jugador1.toLowerCase() == "tijeras" && jugador2.toLowerCase() == "papel")||
-    (jugador1.toLowerCase() == "piedra" && jugador2.toLowerCase() == "tijeras"))
-{
-    alert("Jugador 1 es el ganador");
-} else  {
-    alert("Jugador 2 es el ganador");
+if (jugador1.toLowerCase() == jugador2.toLowerCase()) {
+    
+    contenedorResultado.innerHTML = "<p>¡Empate!<p>";
+} else if (
+    (jugador1.toLowerCase() == "papel" && jugador2.toLowerCase() == "piedra") ||
+    (jugador1.toLowerCase() == "tijeras" && jugador2.toLowerCase() == "papel") ||
+    (jugador1.toLowerCase() == "piedra" && jugador2.toLowerCase() == "tijeras")
+) {
+    
+    contenedorResultado.innerHTML = "<p>¡Jugador 1 ha ganado!<p>";
+} else {
+    
+    contenedorResultado.innerHTML = "<p>¡Jugador 2 ha ganado!<p>";
 }
 
 
